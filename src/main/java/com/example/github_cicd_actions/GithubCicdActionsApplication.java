62947df5984f52e99bb82dc8bc9d94f670d7aeac
haphong463 <haphong463@gmail.com>
2024-11-26 19:53:54 +0700

    code

---
+++ src/main/java/com/example/github_cicd_actions/GithubCicdActionsApplication.java
@@ -0,0 +1,31 @@
+package com.example.github_cicd_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Hello World!";
+	}
+
+	/*
+		echo "# github-actions-example" >> README.md
+		git init
+		git add README.md
+		git commit -m "first commit"
+		git branch -M main
+		git remote add origin https://github.com/haphong463/github-actions-example.git
+		git push -u origin main
+	*/
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
